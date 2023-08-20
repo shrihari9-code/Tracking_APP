0017f83b8c62ac10ae67bc924fdf6f9159b56693
shrihari <shrihari.shurpali@gmail.com>
2023-08-20 12:41:02 +0530

    removed api key

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App',
+      title: 'Bus_App',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
